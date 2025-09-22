18bf7a75e638e20df739274dcbdb115ea7bd788a
Брешенков Артём Сергеевич <breshenkov_as@electronxray.com>
2025-09-22 14:32:10 +0300

    init

---
+++ CodeLibrary/src/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CodeLibrary;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}

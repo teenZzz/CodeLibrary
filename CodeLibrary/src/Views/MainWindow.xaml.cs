adf4504cd98e50905a10d0e7ce3b8aee3324f00e
Брешенков Артём Сергеевич <breshenkov_as@electronxray.com>
2025-09-24 17:00:56 +0300

    Window development has begun

--- CodeLibrary/src/Views/MainWindow.xaml.cs
+++ CodeLibrary/src/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace CodeLibrary.Views;
 
@@ -10,5 +11,36 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+    
+    private void Minimize_Click(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized; // свернуть окно
+    }
+
+    private void Close_Click(object sender, RoutedEventArgs e)
+    {
+        // Закрыть окно (если это MainWindow и ShutdownMode=OnMainWindowClose — приложение завершится)
+        this.Close();
+
+        // Если хочешь закрывать приложение всегда, даже при открытых окнах:
+        // Application.Current.Shutdown();
+    }
+    
+    private void SearchMouseDown(object sender, MouseButtonEventArgs e)
+    {
+        SearchBox.Focus();
+    }
+    
+    private void SearchTextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+    {
+        if (!string.IsNullOrEmpty(SearchBox.Text) && SearchBox.Text.Length > 0)
+        {
+            SearchBlock.Visibility = Visibility.Collapsed;
+        }
+        else
+        {
+            SearchBlock.Visibility = Visibility.Visible;
+        }
     }
 }
